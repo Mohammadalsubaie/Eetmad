8e6bcdf64db996919629e92da367b455c30fcdc6
Hamad Alharbi <122835197+HamadAlharbi1@users.noreply.github.com>
2025-11-26 22:15:01 +0300

    Update page.tsx

--- frontend/eetmad/src/app/[locale]/page.tsx
+++ frontend/eetmad/src/app/[locale]/page.tsx
@@ -46,16 +46,8 @@ const FAQSection = createDynamicImport(() => import('@/components/features/home/
 export default function HomePage() {
   return (
     <main>
-      <HeroSection />
-      <PlatformOverviewSection />
-      <GettingStartedSection />
-      <AudienceSection />
-      <CTASection />
-      <ExploreCategoriesSection />
-      <ProjectBenefitsSection />
-      <TransparencySection />
-      <SuccessStoriesSection />
-      <FAQSection />
+
+
     </main>
   );
 }
